f8c9add55c2906f67f71af3b27eac0128f1e840d
vieira <vieiranetoc@gmail.com>
2016-08-13 17:11:42 -0300

    Simple site django based

---
+++ 130816/DojoBlog/DojoBlog/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tdd_dojo project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tdd_dojo.settings")
+
+application = get_wsgi_application()
