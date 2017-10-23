720c72ccdc5e313c8952f63e611026bb5a8ceba3
Day <dayannefernandesc@gmail.com>
2017-10-23 18:02:09 -0200

    Finishing 2nd session of Dojo

--- 2017/171023/dojo/main.py
+++ 2017/171023/dojo/main.py
@@ -1,20 +1,51 @@
 def converter(horario):
-    hour, minute = horario.split(':')
-    hour_minute = list(map(int,[hour,minute]))
+    hour, minute_e_utc = horario.split(':')
+    minute, utc = minute_e_utc.split(' ')
+    valor_do_utc = utc[3:]
+    sinal_do_utc = valor_do_utc[1]
 
-    numbers_table = {1: 'uma', 2: 'dois', 3: 'três', 4: 'quatro', 5: 'cinco', 6: 'seis'}
-    decimal_table = {5: 'cinquenta'}
-    minute_table = {0:'',
-                    1: ' e um minuto',
-                    56:' e cinquenta e seis minutos',
-                    15: ' e quinze minutos',
-                    30: ' e trinta minutos'}
+    if sinal_do_utc == '+':
+        somar_na_hora = valor_do_utc[2] + hour
+
+    else:
+        subtrair_na_hora = hour - valor_do_utc[2]
+
+    hour_minute = list(map(int,[hour,minute]))
 
     if hour_minute[0] < 1 or hour_minute[0] > 11 or hour_minute[1] < 0 or hour_minute[1] > 60:
         return 'error'
     else:
-        if hour_minute[0] == 1:
-            hora = ' hora'
+        if hour_minute[1] > 39:
+            number = 60 - hour_minute[1]
+            return numeroliteral(number) + ' minutos para as ' + numeroliteral(hour_minute[0] + 1)
+        elif hour_minute[1] == 30:
+            if hour_minute[0] == 1:
+                return 'uma e meia'
+            else:
+                return numeroliteral(hour_minute[0]) + ' e meia'
         else:
-            hora = ' horas'
-        return numbers_table[hour_minute[0]] + hora + minute_table[hour_minute[1]]
+            if hour_minute[0] == 1:
+                hora = 'uma hora'
+            else:
+                hora = numeroliteral(hour_minute[0]) + ' horas'
+
+            minutos = ''
+            if hour_minute[1] == 1:
+                minutos = ' e um minuto'
+            elif hour_minute[1] > 1:
+                minutos = ' e ' + numeroliteral(hour_minute[1]) + ' minutos'
+
+            return hora + minutos
+
+def numeroliteral(minuto):
+    numbers_table = {
+        0: '', 1: 'um', 2: 'dois', 3: 'três', 4: 'quatro', 5: 'cinco', 6: 'seis', 7: 'sete', 8: 'oito', 9: 'nove', 10: 'dez', 11: 'onze',
+        12: 'doze', 13: 'treze', 14: 'catorze', 15: 'quinze', 16: 'dezesseis', 17: 'dezessete', 18: 'dezoito', 19: 'dezenove', 20: 'vinte', 30: 'trinta', 40: 'quarenta', 50: 'cinquenta'
+    }
+
+    if minuto < 20:
+        return numbers_table[minuto]
+    elif minuto < 40 and minuto%10 == 0:
+        return numbers_table[(minuto)]
+    elif minuto < 40 and minuto%10 != 0:
+        return numbers_table[(minuto - minuto%10)] + ' e ' + numbers_table[minuto%10]
--- 2017/171023/dojo/tests.py
+++ 2017/171023/dojo/tests.py
@@ -20,12 +20,37 @@ class DojoTests(unittest.TestCase):
     def test_a_entrada_eh_seis_e_56(self):
         horario = '06:56'
         saida = main.converter(horario)
-        self.assertEqual(saida, 'seis horas e cinquenta e seis minutos')
+        self.assertEqual(saida, 'quatro minutos para as sete')
 
     def test_a_entrada_vale_uma_hora_e_quinze(self):
         horario = '01:15'
         saida = main.converter(horario)
         self.assertEqual(saida, 'uma hora e quinze minutos')
+
+    def test_quatro_horas_e_trinta(self):
+        horario = '04:30'
+        saida = main.converter(horario)
+        self.assertEqual(saida, 'quatro e meia')
+
+    def test_a_entrada_vale_um(self):
+        horario = 1
+        saida = main.numeroliteral(horario)
+        self.assertEqual(saida, 'um')
+
+    def test_a_entrada_vale_uma_e_meia(self):
+        horario = '01:30'
+        saida = main.converter(horario)
+        self.assertEqual(saida, 'uma e meia')
+
+    def test_a_entrada_vale_vinte_para_as_cinco(self):
+        horario = '04:40'
+        saida = main.converter(horario)
+        self.assertEqual(saida, 'vinte minutos para as cinco')
+
+    def test_a_entrada_3_horas_utc_menos_1(self):
+        horario = '03:00 UTC-1'
+        saida = main.converter(horario)
+        self.assertEqual(saida, 'duas horas')
 
 if __name__ == '__main__':
     unittest.main()
